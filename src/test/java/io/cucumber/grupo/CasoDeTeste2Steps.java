277bc6660c2d4182f7baafe0d699ecc96d5e9749
Manoela <manoela.pala@gmail.com>
2021-03-31 18:51:53 -0300

    atualizando o steps 2

--- src/test/java/io/cucumber/grupo/CasoDeTeste2Steps.java
+++ src/test/java/io/cucumber/grupo/CasoDeTeste2Steps.java
@@ -23,27 +23,55 @@ public class CasoDeTeste2Steps {
 		Thread.sleep(2000);
 	}
 
-	@Entao("devo ver os servicos abaixo")
-	public void devo_ver_os_servicos_abaixo(io.cucumber.datatable.DataTable dataTable) throws InterruptedException {
-		Thread.sleep(5000);
+	@Entao("devo ver os itens de servicos")
+	public void devo_ver_os_itens_de_servicos(io.cucumber.datatable.DataTable dataTable) throws InterruptedException {
+			Thread.sleep(5000);
+		    //Busca dentro da div primaryLink2_Servios o elemento <ul></ul>
 	    	By mySelector = By.xpath("//*[@id=\"primaryLink2_Servios\"]/div/div/ul");
+
+			//Através deste mySelector (<ul></ul>), identifica a quantidade de (<li></li>)
+			// Montando uma lista (myElements) com a quantidade de <li></li>
+			//String[] menuElements - Array
 	    	List<WebElement> myElements = Configuracao.browser.findElements(mySelector);  
-	    
+
+    
+			// Através da Lista myElements, percorre todos os itens (<li></li>) (22)
 	    	for(WebElement e : myElements) {
-	    		String aux = e.getText();    	
-	    		menuElements = aux.split("\n");	    		
+				//Para cada item da lista (<li></li>)
+				
+				//Armazena dentro da variavel (textLine) o text (ex: Accenture Strategy / Business Process Services / ....)
+	    		String textLine = e.getText(); 
+				
+				//escreve a mensagem no console.
+	    		System.out.println(textLine);
+	    	
+				//a funcao split, faz a quebra da linha onde tiver espaço (\n)
+				//e alimenta o array (menuElements)
+				//(ex: Accenture Strategy / Business Process Services / ....)
+				//fica assim
+				//Accenture Strategy
+				//Business Process Services
+	    		menuElements = textLine.split("\n");	    		
 	    	}
 	    
+			int contador;
+			//Percorre todos os itens do array String[] menuElements	
 	    	for (int i = 0; i < menuElements.length; i++) {
-	    		String line = dataTable.row(i).get(0);
-	    		if(menuElements[i].equals(line)) {
-	    			counter++;
+	    		String linha = dataTable.row(i).get(0);
+	    		
+				
+				
+				//Compara os itens da dataTable com os elementos do array 
+	    		if(menuElements[i].equals(linha)) {
+					//Se forem iguais, alimenta a variavel contador com cada item
+	    			contador++;
 	    		}
-		}
-	    	assertEquals(22, counter);
+			}
+		    //valida se a variavel contador é (igual) = 22 e retorna true.
+	    	assertEquals(22, contador);
 	    	Configuracao.fechar();
-
 	}
+}
 
 	@E("clico no item do menu cloud")
 	public void clico_no_clico_no_item_do_menu_cloud() throws InterruptedException {
